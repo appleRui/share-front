a49894d1b21c0386fb058105caea8833d34fc5f7
appleRui <apple31581@gmail.com>
2021-09-21 16:11:50 +0900

    feature/210921/login

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
   <div>
     <div class="post-from">
       <input v-model="post" class="content" type="text">
-      <label><input type="submit" value="送信" @click="store"></label>
+      <label><input type="submit" value="送信" @click="storePost"></label>
     </div>
     <div class="post-conte">
       <ul class="post-items" v-for="post in posts">
@@ -25,17 +25,23 @@ export default {
   data() {
     return{
       userName: null,
+      user_id: null,
       email: null,
       post: null,
       posts: []
     }
   },
   methods: {
-    store() {
+    async storePost() {
+      const sendData = {
+        post: this.post,
+        user_id: this.user_id
+      };
+      console.log(sendData);
       if(this.post !== ''){
-          this.posts.push(this.post)
-          this.post = ''
-      }
+        await this.$axios.post("http://127.0.0.1:8000/api/v1/post/", sendData);
+        this.post = ''
+      };
     },
     logout() {
       firebase.auth().signOut().then(() => {
@@ -48,7 +54,9 @@ export default {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.userName = user.displayName
+        this.user_id = user.uid
         console.log(user.email);
+        console.log(user.uid);
       }else{
         this.$router.replace('/login')
       }
